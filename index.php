68d01f1a5419f407b31186ddea6ddb2679cc7d56
DannHigashi <dann.higashi@inacapmail.cl>
2022-07-07 14:22:41 -0400

    Maquetacion solo Html

--- index.php
+++ index.php
@@ -8,21 +8,80 @@
 </head>
 <body>
     <!--Cabecera-->
-    <header>
+    <header id="header">
+        <!-- logo-->
         <div id="logo">
             <a href="index.php">
                 Blog de Videojuegos
             </a>
         </div>
+        <!--Menu-->
+        <nav id="nav">
+            <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="">Categoria 1</a></li>
+            <li><a href="">Categoria 2</a></li>
+            <li><a href="">Categoria 3</a></li>
+            <li><a href="">Categoria 4</a></li>
+            <li><a href="index.php">Sobre Mi</a></li>
+            <li><a href="index.php">Contacto</a></li>
+            </ul>
+        </nav>
+
+
     </header>
     
-    <!--Menu-->
-    <div></div>
+    <div id="container">
+        <!--Barra Lateral-->
+        <aside id="sidebar">
+            <div id="login" class="block-aside">
+                <h3>Identificate</h3>
+                <form action="Login.php" method="POST">
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
+                    <input type="submit" value="Entrar">
 
+                </form>
+            </div>
+            <div id="register" class="block-aside">
+                <h3>Registrate</h3>
+                <form action="register.php" method="POST">
+                    <label for="nombre">Nombre</label>
+                    <input type="text" name="nombre">
 
-    <!--Barra Lateral-->
-    <!--Cotenido principal-->
+                    <label for="apellidos">Apellidos</label>
+                    <input type="email" name="email">
+
+                    <label for="email">Email</label>
+                    <input type="email" name="email">
+
+                    <label for="password">Contraseña</label>
+                    <input type="password" name="password">
+
+                    <input type="submit" value="Entrar">
+
+                </form>
+            </div>
+        </aside>
+        <!--Contenido principal-->
+        <div id="mail-content">
+            <h1>Ultimas entradas</h1>
+            <article class="art-recents">
+                <h2>Titulo de la entrada</h2>
+                <p>
+                    Descripcion de la entrada
+                </p>
+            </article>
+
+        </div>
+    </div>
+
     <!--Pie de pagina-->
+    <footer>
+    <p>Desarollado por Dann Higashi &COPY; 2022</p>
+    </footer>
     
 </body>
 </html>
